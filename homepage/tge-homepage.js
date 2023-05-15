6e001b80550aa40eb4a07002ab9602e766ee3d76
Amanda Quah <amanda4quah@gmail.com>
2023-05-15 20:10:35 +1000

    Week 11 Edits in Class
    Mainly homepage additions, plus master styles folder added for css things that need to apply to every page

---
+++ homepage/tge-homepage.js
@@ -0,0 +1,10 @@
+// function toggleResponsive(){
+//     var nav = document.getElementById("shop-dropdown")
+//     nav.classList.toggle("shop-dropdown-display")
+// }
+
+function toggleResponsive(){
+    var nav = document.getElementById("shop-dropdown")
+    nav.className="shop-dropdown-display"
+    console.log(working)
+}
