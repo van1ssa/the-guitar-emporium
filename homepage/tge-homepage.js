fef13cff9d7c295912d6a28ebe7133f9abf4002f
Amanda Quah <amanda4quah@gmail.com>
2023-06-05 16:47:01 +1000

    Cart Page Visuals
    All cart page appearance done.

---
+++ homepage/tge-homepage.js
@@ -0,0 +1,11 @@
+// const nav = document.querySelector("shop-dropdown");
+
+// function hoverDropdown(){
+//     console.log("working")
+//     if(nav.getElementById("hide")){
+//     nav.getElementById("show")
+//     }
+//     if(nav.getElementById("show")){
+//         nav.getElementById("hide")
+//         }
+// }
